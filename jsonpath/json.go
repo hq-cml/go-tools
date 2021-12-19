304d76c72c10fa19d35fbcb97d147e2e9ca049cd
hq <hq_cml@163.com>
2021-12-19 11:18:52 +0800

    json path

--- jsonpath/json.go
+++ jsonpath/json.go
@@ -1,22 +1,24 @@
 /*
- Operator	    Supported	Description
-       $         Y	         json的根节点. 通常在表达式开始位置.
-       @         Y           The current node being processed by a filter predicate.
-       *         X           Wildcard. Available anywhere a name or numeric are required.
-       ..        X           Deep scan. Available anywhere a name is required.
-       .         Y           Dot-notated child
-  ['' (, '')]    X           Bracket-notated child or children
-  [ (, )]        Y           Array index or indexes
-  [start:end]    Y           Array slice operator
-  [?()]          Y           Filter expression. Expression must evaluate to a boolean value.
+ * 在不知道具体的json结构的时候，可以用json path来尝试取出值
+ *
+ * Operator	    Supported	Description
+ *      $         Y	         json 的根节点. 通常在表达式开始位置.
+ *      @         Y           The current node being processed by a filter predicate.
+ *      *         X           Wildcard. Available anywhere a name or numeric are required.
+ *      ..        X           Deep scan. Available anywhere a name is required.
+ *      .         Y           Dot-notated child
+ * ['' (, '')]    X           Bracket-notated child or children
+ * [ (, )]        Y           Array index or indexes
+ * [start:end]    Y           Array slice operator
+ * [?()]          Y           Filter expression. Expression must evaluate to a boolean value.
+ *
+ * Note: golang 支持正则表达式标志，格式如 (?imsU)pattern
  */
 package jsonpath
 
 import (
 	"encoding/json"
-	"fmt"
 	"github.com/oliveagle/jsonpath"
-	"reflect"
 )
 
 // 根据路径找到指定的值
@@ -32,41 +34,26 @@ func pickValByPath(dataStr string, path string) (interface{}, error){
 	if err != nil {
 		return nil, err
 	}
-	fmt.Printf("step 1 res: %v, path:%v, type:%v\n",
-		res,  path, reflect.TypeOf(res))
 	return res, nil
 }
 
 // 条件过滤
-func filterByPredicate(dataStr string, filterStr string)  {
+func filterByPredicate(dataStr string, filterStr string) (interface{}, error) {
 	var jsonData interface{}
 	err := json.Unmarshal([]byte(dataStr), &jsonData)
 	if err != nil {
-		fmt.Println(err)
-		return
+		return nil, err
 	}
 
 	//or reuse lookup pattern
 	pattern, err := jsonpath.Compile(filterStr)
 	if err != nil {
-		fmt.Println(err)
-		return
+		return nil, err
 	}
 	res, err := pattern.Lookup(jsonData)
 	if err != nil {
-		fmt.Println(err)
-		return
+		return nil, err
 	}
 
-	switch data := res.(type) {
-	case []interface{}:
-		fmt.Printf("filter res: %v, Type:[]interface{}, len: %d\n",
-			data, len(data))
-		for k, v := range data {
-			fmt.Println("   ", k, ":", v)
-		}
-	default:
-		fmt.Printf("filter res: %v, Type: %v\n", data, reflect.TypeOf(res))
-	}
-
+	return res, nil
 }
--- jsonpath/json_test.go
+++ jsonpath/json_test.go
@@ -1,6 +1,9 @@
 package jsonpath
 
-import "testing"
+import (
+	"reflect"
+	"testing"
+)
 
 var dataStr = `
 {
@@ -43,12 +46,66 @@ var dataStr = `
 `
 
 func Test_pickIntVal(t *testing.T) {
-	pickValByPath(dataStr, "$.expensive")
+	v, err := pickValByPath(dataStr, "$.expensive") // 存在
+	if err != nil {
+		t.Logf("Error: %v", err)
+	}
+	t.Logf("$.expensive: %v, type: %v", v, reflect.TypeOf(v))
+
+	v, err = pickValByPath(dataStr, "$.expensive123") // 不存在
+	if err != nil {
+		t.Logf("Error: %v", err)
+	}
+	t.Logf("$.expensive: %v, type: %v", v, reflect.TypeOf(v))
+
+	v, err = pickValByPath(dataStr, "$.store.book") // 多层路径，获取到一个数组
+	if err != nil {
+		t.Logf("Error: %v", err)
+	}
+	t.Logf("$.expensive: %v, type: %v", v, reflect.TypeOf(v))
 }
 
 func Test_filterByPredicate(t *testing.T) {
-	filterByPredicate(dataStr, `$.store.book[?(@.price < $.expensive)].price`)
-	filterByPredicate(dataStr, `$.store.book[?(@.price > 10)].title`)
-	filterByPredicate(dataStr, `$.store.book[0:2].price`)
-	filterByPredicate(dataStr, `$.expensive`)  // 10
+	filter := func(dataStr string, filterStr string) {
+		res, err := filterByPredicate(dataStr, filterStr)
+		if err != nil {
+			t.Error(err)
+		}
+		switch data := res.(type) {
+		case []interface{}:
+			t.Logf("filter Type:[]interface{}, len: %d. filter:%v",
+				len(data), filterStr)
+			for k, v := range data {
+				t.Log("   ", k, ":", v)
+			}
+		default:
+			t.Logf("filter Type: %v, res: %v, filter:%v", reflect.TypeOf(res), data, filterStr)
+		}
+	}
+
+
+	filter(dataStr,
+		`$.expensive`) // 10
+	filter(dataStr,
+		`$.store.book[0].price`) // 8.95
+	filter(dataStr,
+		`$.store.book[-1].isbn`) // 	"0-395-19395-8"
+	filter(dataStr,
+		`$.store.book[0,1].price`) // [8.95, 12.99]
+	filter(dataStr,
+		`$.store.book[0:2].price`) // [8.95, 12.99, 8.99]
+	filter(dataStr,
+		`$.store.book[:].price`)  	//全部：[8.9.5, 12.99, 8.9.9, 22.99]
+	filter(dataStr,
+		`$.store.book[?(@.price > 10)].title`) 	//["Sword of Honour", "The Lord of the Rings"]
+	filter(dataStr,
+		`$.store.book[?(@.author =~ /(?i).*REES/)].author`) // 正则
+
+
+	// 失败，原因暂时未知
+	t.Log("---------------------------------")
+	filter(dataStr,
+		`$.store.book[?(@.isbn)].price]`) // [8.99, 22.99] (ISBN非空的)
+	filter(dataStr,
+	`$.store.book[?(@.price < $.expensive)].price`)
 }
