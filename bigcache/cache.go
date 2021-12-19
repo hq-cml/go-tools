1075694237b6fa387b8f71ca613ea98da7eae629
hq <hq_cml@163.com>
2021-12-19 15:30:40 +0800

    big cache

--- bigcache/cache.go
+++ bigcache/cache.go
@@ -1,39 +1,46 @@
+/*
+ * 本地缓存
+ * 在高并发的系统中，缓存的设计，应该是分级的，所以本地缓存有其存在价值
+ */
 package bigcache
 
 import (
-    "github.com/allegro/bigcache/v3"
-    "time"
+	"github.com/allegro/bigcache/v3"
+	"time"
 )
 
 type Cache interface {
-    Set()
-    Get()
+	Set(k string, v []byte) error
+	Get(k string) ([]byte, error)
 }
 
 type cache struct {
-    cache *bigcache.BigCache
+	cache *bigcache.BigCache
 }
 
-func NewCache() (Cache, error) {
-    c, err := bigcache.NewBigCache(bigcache.Config{
-        Shards:             128,
-        LifeWindow:         5 * time.Minute,
-        CleanWindow:        5 * time.Minute,
-        MaxEntriesInWindow: 1000 * 10 * 60,
-        MaxEntrySize:       4096,
-        StatsEnabled:       false,
-        // Verbose:            true,
-    })
-    if err != nil {
-        return nil, err
-    }
-    return &cache{cache:c}, nil
+// 这些参数组合起来，会影响到整体内存占用，实际使用中，可以多尝试配置
+func NewCache(shard, maxEntriesInWindow, MaxEntrySize int, lifeWindow, cleanWindow time.Duration) (Cache, error) {
+	c, err := bigcache.NewBigCache(bigcache.Config{
+		Shards:             shard,
+		LifeWindow:         lifeWindow,         // 超过这个时间，条目可以被删除（仅仅是可以，还需要搭配CleanWindow）
+		CleanWindow:        cleanWindow,        // 配合LifeWindow使用，才能起到过期的效果
+		MaxEntriesInWindow: maxEntriesInWindow, // lifewindow内最大条目数
+		MaxEntrySize:       MaxEntrySize,       // 单位：byte，条目最大长度
+
+		// HardMaxCacheSize: 8192, //单位：MB，硬编码写死最大内存占用
+		// CleanWindow:        5 * time.Minute, // 清理过期内存的间隔
+		// Verbose:            true,
+	})
+	if err != nil {
+		return nil, err
+	}
+	return &cache{cache: c}, nil
 }
 
-func (c *cache)Get() {
-
+func (c *cache) Set(k string, v []byte) error {
+	return c.cache.Set(k, v)
 }
 
-func (c *cache)Set() {
-
+func (c *cache) Get(k string) ([]byte, error) {
+	return c.cache.Get(k)
 }
--- bigcache/cache_test.go
+++ bigcache/cache_test.go
@@ -1,13 +1,41 @@
 package bigcache
 
 import (
-    "testing"
+	"log"
+	"testing"
+	"time"
 )
 
-func TestNewCache(t *testing.T) {
-    got, err := NewCache()
-    if err != nil {
-        t.Error(err)
-    }
-    _ = got
+func TestCache(t *testing.T) {
+	c, err := NewCache(
+		2,
+		1024*10,
+		4096,
+		5*time.Second,
+		3*time.Second,
+	)
+	if err != nil {
+		t.Error(err)
+	}
+
+	err = c.Set("foo", []byte("bar"))
+	if err != nil {
+		t.Error(err)
+	}
+
+	v1, err := c.Get("foo")
+	if err != nil {
+		t.Error(err)
+	}
+	log.Printf("Val:%v", string(v1))
+
+	time.Sleep(6 * time.Second)
+	log.Printf("Wake up!")
+
+	// 过期
+	v2, err := c.Get("foo")
+	if err != nil {
+		t.Error(err)
+	}
+	log.Printf("Val:%v", string(v2))
 }
